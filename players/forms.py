1e2e6da1774db5e5339c50ebd7a0aafb97df39c2
roma <romhedoo@gmail.com>
2019-09-29 13:41:33 +0300

    Settings hidden

--- players/forms.py
+++ players/forms.py
@@ -2,6 +2,6 @@ from django import forms
 # from .utils import team_builder
 
 class TeamForm(forms.Form):
-    budget = forms.IntegerField(widget=forms.TextInput(attrs={"placeholder":'Enter your budget in Euros'}))
+    budget = forms.IntegerField(widget=forms.TextInput(attrs={"placeholder":'Enter your budget in €'}))
 
 
